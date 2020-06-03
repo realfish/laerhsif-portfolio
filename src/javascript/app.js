f833619fb40e61a651ec5acc639aa34aad842154
Zhengyu Qian <laerhsif@gmail.com>
2020-06-03 15:21:25 +0800

    Make the scaffold of JS

---
+++ src/javascript/app.js
@@ -0,0 +1,28 @@
+let doc = document;
+
+// Misc
+import _misc from './_misc';
+_misc();
+
+// Class
+
+
+
+// View
+// import viewHttp404 from './view/http404';
+import viewIndex   from './view/index';
+
+// View router
+let view = doc.querySelector('body').classList[0];
+
+switch (view) {
+	// case 'http404': {
+	// 	viewHttp404();
+	// 	break;
+	// }
+	case 'index': {
+		viewIndex();
+		break;
+	}
+}
+
---
+++ src/javascript/view/index.js
@@ -0,0 +1,21 @@
+export default function () {
+
+// let doc = document;
+// let win = window;
+
+
+
+// Init Han.css
+// Han(doc.querySelector('.index-list'))
+// .initCond()
+// .renderElem()
+// .renderHanging()
+// .renderJiya()
+// .renderHWS()
+// /* .correctBasicBD() */
+// /* .substCombLigaWithPUA() */;
+
+
+
+
+}
