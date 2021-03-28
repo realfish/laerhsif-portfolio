672f4635566c33b9ccb61c433701aa11423dafa1
Zhengyu Qian <laerhsif@gmail.com>
2021-03-28 21:43:18 +0800

    Lazy load for artworks

--- src/javascript/view/set.js
+++ src/javascript/view/set.js
@@ -32,4 +32,30 @@ win.addEventListener('resize', _.debounce(function() {
 	}
 }, 400));
 
+
+
+// Image lazy load
+// Ref 1: https://css-tricks.com/tips-for-rolling-your-own-lazy-loading/
+// Ref 2: https://css-tricks.com/the-complete-guide-to-lazy-loading-images/
+let lazyLoad = function (allLazyImgs) {
+	// console.log(allLazyImgs);
+	allLazyImgs.forEach(lazyImg => {
+		if (lazyImg.intersectionRatio > 0) {
+			// console.log(lazyImg);
+			let $lazyImg = lazyImg.target;
+			$lazyImg.src = $lazyImg.dataset.src;
+			lazyImgObserver.unobserve($lazyImg);
+		}
+	});
+};
+let lazyImgObserver = new IntersectionObserver(lazyLoad, {
+	rootMargin: "100px",
+	threshold:  1.0
+});
+
+let $allArtworkImgs = doc.querySelectorAll('.set-artwork img');
+$allArtworkImgs.forEach($artworkImg => {
+	lazyImgObserver.observe($artworkImg);
+});
+
 }
